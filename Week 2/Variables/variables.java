b539cd46b0dc6ba5465d3e46db520a4a6f4b70e7
Your Name <ryanrob327@gmail.com>
2022-10-04 16:07:41 -0700

    completed variables assignment

---
+++ Week 2/Variables/variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String[] args){
+        int age = 16;
+        double height = 5.8;
+        char firstName = 'R';
+        String lastName = "McWeeny";
+        boolean programmingIsBest = true;
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(programmingIsBest);
+    };
+}
